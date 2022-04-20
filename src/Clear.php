44872512082c4e60e91c7db058e367da2daf0770
Maxime Rainville <maxime@silverstripe.com>
2022-04-21 11:30:50 +1200

    MNT Add tests

--- src/Clear.php
+++ src/Clear.php
@@ -7,6 +7,7 @@ use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\GraphQL\Schema\Storage\CodeGenerationStore;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class Clear extends Controller
 {
@@ -26,11 +27,23 @@ class Clear extends Controller
         $expectedPath = BASE_PATH . DIRECTORY_SEPARATOR . $dirName;
         $fs = new Filesystem();
 
+        $finder = new Finder();
+        // Make finder not recursive
+        $finder->depth('== 0');
+
         $logger->info('Clearing GraphQL code generation directory');
         if ($fs->exists($expectedPath)) {
             $logger->info('Directory has been found');
-            $fs->remove($expectedPath);
-            $logger->info('Directory has been removed');
+            if ($finder->in($expectedPath)->hasResults()) {
+                foreach ($finder as $file) {
+                    $logger->info('Removing ' . $file->getFilename());
+                    $fs->remove($file->getRealPath());
+                }
+                $logger->info('Directory now empty');
+            } else {
+                $logger->info('Directory is already empty.');
+            }
+
         } else {
             $logger->info('Directory was not found. There is nothing to clear');
         }
---
+++ tests/ClearTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SilverStripe\GraphQLDevTools\Tests;
+
+use SilverStripe\Dev\FunctionalTest;
+use SilverStripe\GraphQL\Schema\Storage\CodeGenerationStore;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class ClearTest extends FunctionalTest
+{
+
+    private $originalDirName;
+    private $dirName = 'test-graphql-generated';
+    private $absDirName = BASE_PATH . DIRECTORY_SEPARATOR . 'test-graphql-generated';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->originalDirName = CodeGenerationStore::config()->get('dirName');
+        CodeGenerationStore::config()->set('dirName', $this->dirName);
+        $fs = new Filesystem();
+        $fs->mkdir($this->absDirName);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        CodeGenerationStore::config()->set('dirName', $this->originalDirName);
+        $fs = new Filesystem();
+        if ($fs->exists($this->absDirName)) {
+            $fs->remove($this->absDirName)
+        }
+    }
+
+    public function testClear()
+    {
+        $fs = new Filesystem();
+        $finder = new Finder();
+        $finder->in($this->absDirName);
+
+        $this->assertTrue($fs->exists($this->absDirName), 'Test should begin with a fake code gen folder');
+
+        $this->get('dev/graphql/clear');
+        $this->assertFalse($finder->hasResults(), 'GraphQL clear should not break on an empty folder');
+
+        $fs->mkdir($this->absDirName . DIRECTORY_SEPARATOR . 'default');
+        $this->assertTrue($finder->hasResults(), 'A fake schema folder should have been created');
+
+        $this->get('dev/graphql/clear');
+        $this->assertFalse($finder->hasResults(), 'GraphQL clear should have removed the fake schema folder');
+
+        $fs->remove($this->absDirName);
+        $this->get('dev/graphql/clear');
+        $this->assertFalse($fs->exists($this->absDirName), 'GraphQL clear should not break on a non-existent folder');
+    }
+
+}
