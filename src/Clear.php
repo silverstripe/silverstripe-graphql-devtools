cd2dc0193004eeaec865f97e29f9bc7c332cab1e
Maxime Rainville <maxime@rainville.me>
2022-06-08 15:43:29 +1200

    NEW Add a clear task (#40)
    * MNT Add a license to the package
    
    * NEW Clear task for GraphQL v4
    
    * Peer review feedback
    
    Co-authored-by: Steve Boyd <emteknetnz@gmail.com>
    
    * MNT Add tests
    
    * Fix broken syntax
    
    * Remove pointless dev requirement
    
    * Only install v4
    
    * iFix typo
    
    * Explicitely use DB
    
    * Fix broken test
    
    * Update .travis.yml
    
    Co-authored-by: Steve Boyd <emteknetnz@gmail.com>
    
    * DOC Reference the clear task in the doc
    
    Co-authored-by: Steve Boyd <emteknetnz@gmail.com>

---
+++ src/Clear.php
@@ -0,0 +1,49 @@
+<?php
+namespace SilverStripe\GraphQLDevTools;
+
+use Psr\Log\LoggerInterface;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\GraphQL\Schema\Storage\CodeGenerationStore;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class Clear extends Controller
+{
+    private static $url_handlers = [
+        '' => 'clear',
+    ];
+
+    private static $allowed_actions = [
+        'clear',
+    ];
+
+    public function clear(HTTPRequest $request): void
+    {
+        $logger = Injector::inst()->get(LoggerInterface::class . '.graphql-build');
+        $dirName = CodeGenerationStore::config()->get('dirName');
+        $expectedPath = BASE_PATH . DIRECTORY_SEPARATOR . $dirName;
+        $fs = new Filesystem();
+
+        $finder = new Finder();
+        // Make finder not recursive
+        $finder->depth('== 0');
+
+        $logger->info('Clearing GraphQL code generation directory');
+        if ($fs->exists($expectedPath)) {
+            $logger->info('Directory has been found');
+            if ($finder->in($expectedPath)->hasResults()) {
+                foreach ($finder as $file) {
+                    $logger->info('Removing ' . $file->getFilename());
+                    $fs->remove($file->getRealPath());
+                }
+                $logger->info('Directory now empty');
+            } else {
+                $logger->info('Directory is already empty.');
+            }
+        } else {
+            $logger->info('Directory was not found. There is nothing to clear');
+        }
+    }
+}
